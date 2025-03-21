177b715f26348538637e05a4cc73d636e4de5e43
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2025-03-21 14:47:40 +0530

    project completed till milestone 27

--- follow-along-frontend/src/Routes/Routes.jsx
+++ follow-along-frontend/src/Routes/Routes.jsx
@@ -12,3 +12,4 @@ import SelectAddress from "../pages/SelectAddress.jsx";
 import MyOrdersPage from "../pages/myorders.jsx"
 import OrderConfirmation from "../pages/OrderConfirmation.jsx"
 export { Login,Signup,Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile,CreateAddress, SelectAddress, MyOrdersPage, OrderConfirmation}
+
